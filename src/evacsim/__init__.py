ee6ee2b7cd70cc93335663a67b71f63c3622cbb8
Gabriel Perry <perrygabriel99@gmail.com>
2026-01-06 14:28:26 -0700

    Restore snapshot from zip

--- src/evacsim/__init__.py
+++ src/evacsim/__init__.py
@@ -1,6 +1,6 @@
 from .plotting import plot_travel_times_by_policy, plot_network_with_generator_stats
 from .policies import all_open_policy, cycle_policy, greedy_longest_current_wait_policy, longest_current_wait_policy, longest_cumulative_wait_policy
-from .sim import generate_random_DAG, build_road_network, run_multiple_simulations
+from .sim import generate_random_DAG, build_road_network, run_simulation, run_multiple_simulations
 
 __all__ = [
     "plot_travel_times_by_policy",
@@ -12,5 +12,6 @@ __all__ = [
     "longest_cumulative_wait_policy",
     "generate_random_DAG",
     "build_road_network",
+    "run_simulation",
     "run_multiple_simulations",
 ]
